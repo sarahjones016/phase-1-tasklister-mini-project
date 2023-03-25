217d7f84a6954a332af1669bdf4e463aa0dd6867
Sarah Jones <sarahrjones8@gmail.com>
2023-03-25 15:27:45 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector("form")
+  form.addEventListener('submit', function(e) {
+    e.preventDefault()
+    buildToDo(e["target"]["new-task-description"]["value"])
+    //Had to use [] instead of . because the hyphens break the variable name
+    form.reset()
+  })
+})
+
+function buildToDo(toDoInput) {
+  let paragraph = document.createElement("p")
+  let deleteButton = document.createElement("button")
+  let dropdown = document.createElement("select")
+
+  let optionOne = document.createElement("option")
+  let optionTwo = document.createElement("option")
+  let optionThree = document.createElement("option")
+  let allOptions = [optionOne, optionTwo, optionThree]
+
+  optionOne.textContent = "High Prio"
+  optionOne.id = "High Prio"
+  optionTwo.textContent = "Medium Prio"
+  optionTwo.id = "Medium Prio"
+  optionThree.textContent = "Low Prio"
+  optionThree.id = "Low Prio"
+
+  //https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement/selectedOptions
+  
+  function assignColor() {
+    if (dropdown.selectedIndex === 0) {
+      paragraph.style.color = "red";
+    } else if (dropdown.selectedIndex === 1) {
+      paragraph.style.color = "yellow";
+    } else if (dropdown.selectedIndex === 2) {
+      paragraph.style.color = "green";
+    }
+  }
+  
+  dropdown.addEventListener("change", assignColor)
+
+  deleteButton.addEventListener('click', handleDelete)
+  deleteButton.textContent = "x"
+  paragraph.textContent = `${toDoInput}  `
+  paragraph.appendChild(deleteButton)
+  paragraph.appendChild(dropdown)
+  dropdown.appendChild(optionOne)
+  dropdown.appendChild(optionTwo)
+  dropdown.appendChild(optionThree)
+  document.querySelector("#tasks").appendChild(paragraph)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
+
